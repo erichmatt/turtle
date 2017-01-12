dda9a9b89de85be64a2c5b1680093538bc2a4a4f
binarydinosaur252 <cyrusb252@gmail.com>
2017-01-12 09:52:34 -0500

    Added colors and exit on click

--- spiral-maker.py
+++ spiral-maker.py
@@ -4,7 +4,10 @@ from time import sleep
 turtle1 = turtle.Turtle()
 turtle1.color("blue")
 x = 10
-while 1 == 1:
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")
+for d in range(50):
     turtle1.left(137.5)
     turtle1.forward(float(x))
     x = x + 5
+wn.exitonclick()
