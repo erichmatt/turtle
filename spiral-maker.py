c2556a4b8c513415f592362f06dd7a9bfc47e72e
binarydinosaur252 <cyrusb252@gmail.com>
2017-01-12 09:39:02 -0500

    Added new script for making spirals

---
+++ spiral-maker.py
@@ -0,0 +1,10 @@
+import turtle
+from time import sleep
+
+turtle1 = turtle.Turtle()
+turtle1.color("blue")
+x = 10
+while 1 == 1:
+    turtle1.left(137.5)
+    turtle1.forward(float(x))
+    x = x + 5
