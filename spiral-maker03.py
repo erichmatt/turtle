2f062c94a0dfe34ad858e7ba49b46712941754af
binarydinosaur252 <cyrusb252@gmail.com>
2017-01-13 12:21:00 -0500

    added fibonacci spiral maker.

---
+++ spiral-maker03.py
@@ -0,0 +1,13 @@
+import turtle
+from time import sleep
+
+wn = turtle.Screen()
+x = 1
+turtle1 = turtle.Turtle()
+turtle1.color("green")
+for d in range(35):
+    turtle1.forward(x)
+    turtle1.left(90)
+    x = x * 1.1618
+
+wn.exitonclick()
