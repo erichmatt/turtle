4dbaa1ce1764f633a2a733982e400dea72a44a3e
binarydinosaur252 <cyrusb252@gmail.com>
2017-01-14 11:22:12 -0500

    fixed exit on click.

--- spiral-maker03.py
+++ spiral-maker03.py
@@ -5,7 +5,7 @@ wn = turtle.Screen()
 x = 1
 turtle1 = turtle.Turtle()
 turtle1.color("green")
-for d in range(35):
+for d in range(43):
     turtle1.forward(x)
     turtle1.left(90)
     x = x * 1.1618
