fbe675fe88b912ae5e096b43f1a67bc0cd4e9663
binarydinosaur252 <cyrusb252@gmail.com>
2017-01-28 13:10:30 -0500

    added option to type amount of iterations.

--- spiral-makercircle.py
+++ spiral-makercircle.py
@@ -1,11 +1,13 @@
-import turtle
-from time import sleep
 
+d = input("enter amount of iterations: ")
+
+import turtle
 screen = turtle.Screen()
 turtle0 = turtle.Turtle()
 turtle0.color("dark blue")
 x = 2
-for c in range(30):
+
+for c in range(int(d)):
     turtle0.forward(20 + int(x))
     turtle0.left(60)
     x = x + 4
